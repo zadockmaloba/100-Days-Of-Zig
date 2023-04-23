3d2cf35fc5293fd292b2db95bf7eb444e4d5b74d
Zadock Maloba <malobazadock@gmail.com>
2023-04-23 17:14:57 +0300

    Update source

--- Day2/src/main.zig
+++ Day2/src/main.zig
@@ -2,6 +2,8 @@
 
 const std = @import("std");
 
+const cout = std.debug.print;
+
 pub fn main() !void {
     const _str = "The sum of {} and {} is {}";
 
@@ -10,5 +12,21 @@ pub fn main() !void {
 
     const _num3: comptime_int = _num + _num2;
 
+    cout("{}\n", .{@TypeOf(_str)});
+
+    const @"user name" = "Joe Rogan"; //Variable identifier with spaces
+
+    cout("My YouTube account is {s}\n", .{@"user name"});
+
+    const _name = "Steve Jobs";
+
+    print_name(_name);
+
     std.debug.print(comptime _str, .{ _num, _num2, _num3 });
+}
+
+pub fn print_name(name: []const u8) void {
+    cout("Hello {s}\n", .{name});
+
+    cout("Type of param passed: {}\n", .{@TypeOf(name)});
 }
